1a82b236eb845bd457f983bb41fc8b4a0f249261
Lisa <lisa.na2710@gmail.com>
2025-11-04 12:01:15 +0100

    add: popup components

---
+++ frontend/src/score-popup/score-popup.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-score-popup',
+  imports: [],
+  templateUrl: './score-popup.html',
+  styleUrl: './score-popup.css',
+})
+export class ScorePopup {
+
+}
