1d281e8681c75216704b9994c9c79fe9604c4e7c
Lisa <lisa.na2710@gmail.com>
2025-11-13 10:41:58 +0100

    fix: import error

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,26 +1,22 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { FooterComponent } from '../footer/footer.component';
 import { ScoreboardComponent } from '../scoreboard/scoreboard.component';
-import { FormsModule } from '@angular/forms'; // <-- WICHTIG 1: Importieren
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  // WICHTIG 2: Hier 'FormsModule' hinzufügen
-  imports: [FooterComponent, ScoreboardComponent, FormsModule],
+  imports: [ScoreboardComponent, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
-  // WICHTIG 3: Die Eigenschaft hier definieren (Standardwert 6)
   public roundsToPlay: number = 6;
 
   constructor(private router: Router) {}
 
   startGame() {
-    // Navigiert zur Spiel-Seite und übergibt die Rundenanzahl
     this.router.navigate(['/game'], { queryParams: { rounds: this.roundsToPlay } });
   }
 }
