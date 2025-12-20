39fb84472ac3eee22332af4379383f26582531ba
Daniel Ziegler <danielziegler.1@gmx.de>
2025-12-20 22:46:00 +0100

    fix __dirname in game-service express-app.ts

--- backend/game-service/src/api/express-app.ts
+++ backend/game-service/src/api/express-app.ts
@@ -6,7 +6,10 @@ import geoDataRouter from "@/api/geodata-routes";
 import session from 'express-session';
 import dotenv from "dotenv";
 import path from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 declare module 'express-session' {
