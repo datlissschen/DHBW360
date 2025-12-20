ed7eccce0877ca1f76c157df862e286089042a90
Daniel Ziegler <danielziegler.1@gmx.de>
2025-12-20 23:15:41 +0100

    fix frontend prod environment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: true,
-    // Use container names for internal networking
-    gameServiceBaseUrl: 'dhbw360-game-service-1:9501',
-    scoreServiceBaseUrl: 'dhbw360-score-service-1:9502',
+    gameServiceBaseUrl: 'http://vsv-research.volkmann-webservices.de:9501',
+    scoreServiceBaseUrl: 'http://vsv-research.volkmann-webservices.de:9502',
 };
