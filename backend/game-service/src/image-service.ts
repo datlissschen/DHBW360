2c903625b0486274effd3e34057cc9720596eb66
Daniel Ziegler <danielziegler.1@gmx.de>
2025-12-21 17:47:40 +0100

    fix image download path #2

--- backend/game-service/src/image-service.ts
+++ backend/game-service/src/image-service.ts
@@ -1,16 +1,19 @@
 import * as fs from 'fs/promises';
 import path from "path";
 import {downloadFile} from "@/s3";
+import {fileURLToPath} from "url";
 
 export async function ensureImageFile(fileName: string) {
-    const fullFilePath = path.resolve('./game-service/download', fileName);
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const filePath = path.resolve(__dirname, '../download', fileName);
     try {
-        await fs.access(fullFilePath);
+        await fs.access(filePath);
     } catch (error) {
         console.log(`File ${fileName} not found. Start download...`);
         try {
             await downloadFile(fileName);
-            console.log('Download completed. File saved at: ', fullFilePath);
+            console.log('Download completed. File saved at: ', filePath);
         } catch (downloadError) {
             console.error('Error while downloading file:', downloadError);
             throw downloadError;
